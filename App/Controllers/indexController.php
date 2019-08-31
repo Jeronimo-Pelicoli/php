ff9cb3d5e56fef89e55b6e8220d65991196fc783
jeronimo-peicoli <jeronimosama@gmail.com>
2019-08-30 21:32:54 -0300

    first commit

---
+++ App/Controllers/indexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+    public function index()
+    {
+        echo "Route: / controller: index";
+    }
+
+    public function contact()
+    {
+        echo "Route: /contact controller: contact";
+    }
+}
+?>
---
+++ App/Route.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+class Route
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $this->initRoutes();
+        $this->run($this->getUrl());
+    }
+
+    public function initRoutes()
+    {
+        $routes['home'] = array('route'=>'/', 'controller'=>"indexController", 'action'=>'index');
+        $routes['contact'] = array('route'=>'/contact', 'controller'=>"indexController", 'action'=>'contact');
+        $this->setRoutes($routes);
+    }
+
+    public function run($url)
+    {
+        array_walk($this->routes, function($route) use ($url) {
+            if($url == $route['route']) {
+                $class = "App\\Controllers\\".ucfirst($route['controller']);
+                $controller = new $class;
+                $action = $route['action'];
+                $controller->$action();
+            }
+        });
+    }
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function getUrl()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+}
+?>
