53c8f12aabe88436d74342a8d7037976353ff285
jeronimo-peicoli <jeronimosama@gmail.com>
2019-10-08 16:40:04 -0300

    layout

--- vendor/SON/Controller/Action.php
+++ vendor/SON/Controller/Action.php
@@ -12,9 +12,19 @@ abstract class Action
         $this->view = new \stdClass;
     }
 
-    Protected function render($action)
+    Protected function render($action, $layout = true)
     {
         $this->action = $action;
+        if($layout == true && file_exists("../App/Views/layout.phtml")) {
+            include_once "../App/Views/layout.phtml";
+        } else {
+            $this->content();
+        }
+        
+    }
+
+    protected function content()
+    {
         $current = get_class($this);
         $singleClassName = strtolower((str_replace("Controller", "", str_replace("App\\Controllers\\", "", $current))));
         include_once "../App/Views/" . $singleClassName . "/" . $this->action . ".phtml";
